8ac7e2f48685ff9d180f01e0b6cd163766fb350a
wowbat <weiwei3381@live.com>
2019-04-11 20:00:39 +0800

    React中使用CSS动画效果

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import TodoList from './TodoList';
+import App from './App';
 // 使用了JSX语法,所以在本页面需要引入React
 // JSX语法中，如果使用自己定义的组件，需要以大写字母开头
-ReactDOM.render(<TodoList />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
